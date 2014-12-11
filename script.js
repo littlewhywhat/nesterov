cb4f51f330985305276d3813164b88d0c23a8c9d
roman <littlewhywhat@gmail.com>
2014-12-11 19:57:17 +0300

    decoupled image-related logic to class Images 	modified:   script.js: Images: added, 			       selectImageByCircle: removed, 			       selectImageById: added, 			       renamed images to IMAGE_IDS, 			       added images as instance of Images

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var SVG_ID = "content",
     INDICATOR_ID = "indicator",
     COVER_ID = "cover",
     CIRCLE_TAG = "circle",
-    IMAGE_TAG = "image",
     SELECTED_CLASS = "selected",
     BUTTON_COUNT = 3,
     BUTTON_RADIUS = 10,
@@ -11,12 +10,87 @@ var SVG_ID = "content",
     width = document.body.clientWidth,
     minwidth = 1920,
     minheight = 966,
-    images = ["zero", "one", "two"];
+    IMAGE_IDS = ["zero", "one", "two"];
 
 var s = Snap(formId(SVG_ID));
 var interval = 500;
     imageInterval = 4000;
 var loading = true;
+
+var images;
+function Images(s, IMAGE_IDS) {
+    var IMAGE_TAG = "image",
+        instance = this;
+    this.init = function() {
+        for (var i = 0, len = IMAGE_IDS.length; 
+                        i < len; i++)
+            createImage(IMAGE_IDS[i], null);
+        return instance;
+    }
+
+    function createImage(image, interval) {
+        var image = drawImage(image);
+        adjustImage(image, width, height);
+        instance.selectImage(image);
+    }
+
+    function drawImage(image) {
+        return s.image(getImageURL(image), 0, 0, 0, 0)
+                     .attr({
+                        id: image
+                     });
+    }
+
+    function adjustImage(image, width, height) {
+        var rate = width/height;
+        var adjheight = height;
+        var adjwidth = width;
+        if (rate > 2)
+            adjheight = adjwidth /2;
+        if (rate < 2)
+            adjwidth = adjheight * 2;
+        image.attr({
+            height: adjheight,
+            width: adjwidth
+        });
+        var center = width/2 - adjwidth/2;
+        image.transform("t" + [center, 0]);
+    }
+
+    this.adjust = function() {
+        getImageSet().forEach(function(image) {
+            adjustImage(image, width, height);
+        })
+    }
+
+    this.selectImage = function(image) {
+        var selected = instance.getSelectedImage();
+        if (selected)
+            toggleSelected(selected);
+        getCover().before(image);
+        toggleSelected(image);
+    }   
+
+    function getImageURL(id) {
+        return id + ".jpg";
+    }
+
+    function getImageSet() {
+        return s.selectAll(IMAGE_TAG);
+    }
+
+    this.getImageById = function(id) {
+        return s.select(IMAGE_TAG + formId(id));
+    }
+
+    this.getSelectedImage = function() {
+        return s.select(IMAGE_TAG + cssClass(SELECTED_CLASS));
+    }
+
+    this.selectImageById = function(id) {
+        instance.selectImage(instance.getImageById(id));
+    }
+}
 
 doInARow([{
     time: interval,
@@ -29,24 +103,23 @@ doInARow([{
     todo: uncover 
 }, { 
     time: imageInterval , 
-    todo: load, args: images 
+    todo: load, args: IMAGE_IDS 
 }, { 
     time: interval, 
     todo: cover
 }]);
 
-function load(images, time) {
+function load(IMAGE_IDS, time) {
     startAnimate(500);
     doInARow([{ 
             time: 0, 
             todo: stopAnimate
         }, { 
             time: time,
-            todo: function(images, time) {
-                while (images.length > 0)
-                    createImage(images.pop(), null);
+            todo: function(IMAGE_IDS, time) {
+                images = new Images(s, IMAGE_IDS).init();
             },
-            args: images,
+            args: IMAGE_IDS,
     }]);
     
 }
@@ -65,8 +138,8 @@ function doInARow(callbacks) {
 }
 
 function cover(interval) {
-    for (var i in images) 
-        drawCircle(0, 0, images[i]);
+    for (var i in IMAGE_IDS) 
+        drawCircle(0, 0, IMAGE_IDS[i]);
     var indicator = s.g(getCircleSet())
                      .attr({
                         id: INDICATOR_ID
@@ -127,24 +200,8 @@ function getCircleById(id) {
     return s.select(CIRCLE_TAG + formId(id));
 }
 
-function getImageById(id) {
-    return s.select(IMAGE_TAG + formId(id));
-}
-
-function getImageSet() {
-    return s.selectAll(IMAGE_TAG);
-}
-
 function getSelectedCircle() {
     return s.select(CIRCLE_TAG + cssClass(SELECTED_CLASS));
-}
-
-function getSelectedImage() {
-    return s.select(IMAGE_TAG + cssClass(SELECTED_CLASS));
-}
-
-function getImageURL(id) {
-    return id + ".jpg";
 }
 
 function formId(id) {
@@ -159,14 +216,6 @@ function toggleSelected(element) {
     element.toggleClass(SELECTED_CLASS);
 }
 
-function selectImage(image) {
-    var selected = getSelectedImage();
-    if (selected)
-        toggleSelected(selected);
-    getCover().before(image);
-    toggleSelected(image);
-}
-
 function selectCircle(circle) {
     var selected = getSelectedCircle();
     if (selected)
@@ -174,46 +223,11 @@ function selectCircle(circle) {
     toggleSelected(circle);
 }
 
-function selectImageByCircle(circle) {
-    var image = getImageById(circle.attr('id'));
-    selectImage(image);
-}
-
-function createImage(image, interval) {
-    var image = drawImage(image, width, height);
-    selectImage(image);
-}
-
 function drawCover(width, height) {
     return s.rect(0,0,width,height)
             .attr({
                 id: COVER_ID
             });
-}
-
-function drawImage(image, width, height) {
-    var image = s.image(getImageURL(image), 0, 0, width, height)
-                 .attr({
-                    id: image
-                 });
-    adjustImage(image, width, height);
-    return image;
-}
-
-function adjustImage(image, width, height) {
-    var rate = width/height;
-    var adjheight = height;
-    var adjwidth = width;
-    if (rate > 2)
-        adjheight = adjwidth /2;
-    if (rate < 2)
-        adjwidth = adjheight * 2;
-    image.attr({
-        height: adjheight,
-        width: adjwidth
-    });
-    var center = width/2 - adjwidth/2;
-    image.transform("t" + [center, 0]);
 }
 
 function drawCircle(cx, cy, id) {
@@ -231,13 +245,13 @@ function relateCircles() {
             selectByCircle(this); 
         }, function() {});
    });
-   var selectedId = getSelectedImage().attr('id');
+   var selectedId = images.getSelectedImage().attr('id');
    selectCircle(getCircleById(selectedId));
 }
 
 function selectByCircle(circle) {
     selectCircle(circle);
-    selectImageByCircle(circle);
+    images.selectImageById(circle.attr('id'));
 }
 
 function placeCirclesDown(height, width, interval) {
@@ -291,9 +305,7 @@ document.body.onresize = function() {
         cover.attr({
             opacity: 0
         });
-        getImageSet().forEach(function(image) {
-            adjustImage(image, width, height);
-        })
+        images.adjust();
         placeCirclesDown(height, width, 500);
     }, 1000);
     startMillis = currentMillis;
